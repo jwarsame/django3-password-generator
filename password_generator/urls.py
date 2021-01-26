b61c8aa2f40c47deb517013e5d70c07585dfcb84
jwarsame <hi-din@hotmail.com>
2021-01-25 22:09:38 -0600

    i have made some changes in html

--- password_generator/urls.py
+++ password_generator/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from generator import views
 
 urlpatterns = [
-    path('home', views.home, name='home'),
+    path('', views.home, name='home'),
     path('about', views.about, name='about'),
     path('password', views.password, name='password'),
 
